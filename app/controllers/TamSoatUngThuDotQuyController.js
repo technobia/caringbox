630df0232fa2ca115f6b577fcacfd97cb00f4539
binh <technobia@gmail.com>
2016-12-31 12:34:04 +0700

    implement angular

---
+++ app/controllers/TamSoatUngThuDotQuyController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by apium on 12/31/2016.
+ */
+app.controller('TamSoatUngThuDotQuyController', ['$scope', tamSoatUngThuDotQuy]);
+
+function tamSoatUngThuDotQuy($scope) {
+    $scope.data = {};
+    $scope.event = {};
+
+    $scope.data.email = ''
+}
