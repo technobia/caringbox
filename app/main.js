d3418601927c5113c0d247e1b58b6f1ec1fcbd46
binh <technobia@gmail.com>
2017-01-10 21:35:44 +0700

    prepare for new update domain

--- app/main.js
+++ app/main.js
@@ -4,3 +4,9 @@
 var app = angular.module('caringbox', [
     'ui.bootstrap'
 ]);
+
+app.controller('MainController', ['$scope', '$location', MainController]);
+
+function MainController($scope, $location) {
+    if ($location.absUrl().indexOf('localhost') > -1) console.log(true);
+}
