154785fd3fc152e500ba2264bced62df48560a95
binh <technobia@gmail.com>
2017-01-10 22:16:10 +0700

    check domain

--- app/main.js
+++ app/main.js
@@ -10,5 +10,7 @@ app.controller('MainController', ['$scope', '$location', MainController]);
 function MainController($scope, $location) {
     if ($location.absUrl().indexOf('tamsoatungthudotquy.net') > -1) {
         $scope.content = 'tam-soat-ung-thu-dot-quy.html';
+    } else if ($location.absUrl().indexOf('thucphamchongungthu.net') > -1) {
+        $scope.content = 'thuc-pham-chong-ung-thu.html';
     }
 }
