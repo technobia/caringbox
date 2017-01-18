5bb47d9525c190418e5e6a6b986a26f5a521c395
binh <technobia@gmail.com>
2017-01-18 15:17:27 +0700

    desktop finish

--- app/main.js
+++ app/main.js
@@ -10,15 +10,15 @@ app.controller('MainController', ['$scope', '$location', MainController]);
 function MainController($scope, $location) {
     if ($location.absUrl().indexOf('tamsoatungthudotquy.net') > -1) {
         $scope.content = 'tam-soat-ung-thu-dot-quy.html';
-        $scope.title = 'Tầm Soát Ung Thư Đột Quỵ';
+        $scope.isPageOne = true;
         $scope.type = 1;
     } else if ($location.absUrl().indexOf('thucphamchongungthu.net') > -1) {
         $scope.content = 'thuc-pham-chong-ung-thu.html';
-        $scope.title = 'Thực Phẩm Chống Ung Thư';
+        $scope.isPageTwo = true;
         $scope.type = 2;
     } else if ($location.absUrl().indexOf('dulichnuocngoaimienphi.net') > -1) {
         $scope.content = 'du-lich-nuoc-ngoai-mien-phi.html';
-        $scope.title = 'Du Lịch Nước Ngoài Miễn Phí';
+        $scope.isPageThree = true;
         $scope.type = 3;
     }
 }
